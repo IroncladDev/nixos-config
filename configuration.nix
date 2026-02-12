fb23e127348d1b4cd9ab4c31ebaa4de084ff7dec
IroncladDev <conner@ironclad.sh>
2026-02-11 19:32:23 -0600

    fix kanata on asahi nixos

--- configuration.nix
+++ configuration.nix
@@ -6,14 +6,11 @@
       ./hardware-configuration.nix
       ./apple-silicon-support
       ./system.nix
-      ./packages.nix
-      ./services.nix
-      ./programs.nix
     ];
 
-  hardware = {
-    asahi.peripheralFirmwareDirectory = ./firmware;
-  };
+  hardware.asahi.peripheralFirmwareDirectory = ./firmware;
+
+  boot.kernelModules = [ "uinput" ];
 
   xdg.portal = {
     enable = true;
@@ -23,12 +20,70 @@
     ];
   };
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.groups.uinput = {};
   users.users.ironcladdev = {
     isNormalUser = true;
-    extraGroups = [ "wheel" "video" "input" "audio" "networkmanager" ];
+    extraGroups = [ "wheel" "video" "input" "audio" "networkmanager" "uinput" ];
     packages = with pkgs; [
       tree
     ];
   };
+
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
+
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session.command = "tuigreet --cmd Hyprland";
+      terminal = {
+        vt = 1;
+        reset = true;
+        clear = true;
+      };
+    };
+  };
+
+  # services.udev.extraRules = ''
+  #   KERNEL=="uinput", MODE="0660", GROUP="uinput", OPTIONS+="static_node=uinput"
+  # '';
+
+  # For Asahi NixOS specifically
+  services.udev.packages = [ pkgs.udev ];
+  services.udev.extraRules = ''
+    SUBSYSTEM=="input", KERNEL=="uinput", MODE="0660", GROUP="input"
+    SUBSYSTEM=="misc", KERNEL=="uinput", MODE="0660", GROUP="input"
+  '';
+
+  programs.hyprland = {
+    enable = true;
+    withUWSM = true;
+    xwayland.enable = true;
+  };
+
+  programs.starship.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    kitty
+    neovim
+    git
+    yazi
+    (waybar.overrideAttrs (oldAttrs: {
+      mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
+    }))
+    dunst
+    libnotify
+    swww
+    rofi
+    brave
+    magic-wormhole
+    tuigreet
+    gh
+    kanata-with-cmd
+  ];
 }
